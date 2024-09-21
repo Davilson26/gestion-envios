c791182a54a3574bc2c2f4d11686a0e166677ba4
Ana_sofia_castillo <89151060+Ana7w7@users.noreply.github.com>
2024-09-21 16:40:57 -0500

    update routes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,65 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+
+<div class="row col-12">
+  <div class="col-lg-3 col-6">
+   <div class="small-box bg-info">
+        <div class="inner">
+            <h3>150</h3>
+            <p>New Orders</p>
+        </div>
+        <div class="icon">
+            <i class="fas fa-shopping-cart"></i>
+        </div>
+        <a href="#" class="small-box-footer">
+            More info <i class="fas fa-arrow-circle-right"></i>
+        </a>
+    </div>
+</div><div class="col-lg-3 col-6">
+   <div class="small-box bg-success">
+        <div class="inner">
+            <h3>150</h3>
+            <p>New Orders</p>
+        </div>
+        <div class="icon">
+            <i class="fas fa-chart-line"></i>
+        </div>
+        <a href="#" class="small-box-footer">
+            More info <i class="fas fa-arrow-circle-right"></i>
+        </a>
+    </div>
+</div>
+<div class="col-lg-3 col-6">
+   <div class="small-box bg-primary">
+        <div class="inner">
+            <h3>150</h3>
+            <p>New Orders</p>
+        </div>
+        <div class="icon">
+            <i class="fas fa-user-plus"></i>
+        </div>
+        <a href="#" class="small-box-footer">
+            More info <i class="fas fa-arrow-circle-right"></i>
+        </a>
+    </div>
+</div>
+<div class="col-lg-3 col-6">
+   <div class="small-box bg-secondary">
+        <div class="inner">
+            <h3>150</h3>
+            <p>New Orders</p>
+        </div>
+        <div class="icon">
+            <i class="fas fa-flag"></i>
+        </div>
+        <a href="#" class="small-box-footer">
+            More info <i class="fas fa-arrow-circle-right"></i>
+        </a>
+    </div>
+</div>
+</div>
+
 @stop
 
 @section('css')
